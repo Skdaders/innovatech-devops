3b5a9a4022c7b49ee68069da7edc95fc6a97653f
Thomas Manquepan <th.manquepan@duocuc.cl>
2026-05-25 15:17:39 -0400

    update frontend message

--- devops/eva2/devops/devops/trabajo ava2/frontend/src/main.js
+++ devops/eva2/devops/devops/trabajo ava2/frontend/src/main.js
@@ -7,6 +7,6 @@ document.querySelector("#app").innerHTML = `
   <main style="font-family:system-ui;margin:2rem;">
     <h1>Frontend OK</h1>
     <p>VITE_API_URL (build-time): <code>${api || "(no definida)"}</code></p>
-    <p>Este es nuestro Proyecto, Thomas manquepan, khaterine</p>
+    <p>Este es nuestro Proyecto, Thomas manquepan, khaterine Diaz </p>
   </main>
 `;
